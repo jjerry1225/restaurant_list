0c7b7ba3dacaf327145343bc04f444801c1dfa45
jjerry1225 <i183801225@gmail.com>
2022-12-21 16:19:45 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+
+// 引用連線mongoose的檔案，對 app.js 而言，Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數。
+require('./config/mongoose')
 
 // 引用路由器，路徑設定為 /routes 就會自動去尋找目錄下叫做 index 的檔案。
 const routes = require("./routes");
@@ -22,28 +24,6 @@ app.use(express.static("public"));
 
 // 套用method-override
 app.use(methodOverride("_method"));
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
-// connect to mongoose
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// 取得資料庫連線狀態
-const db = mongoose.connection;
-// 連線異常
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-// 連線成功
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
 
 // route setting
 app.use(routes);
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,26 +1,9 @@
-const mongoose = require("mongoose");
+const db = require("../../config/mongoose");
 const Restaurant = require("../restaurant"); // 載入restaurant model
 const restaurantList = require("../../restaurant.json").results; // 載入restaurant.json 的陣列
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
-// connect to mongoose
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// 取得資料庫連線狀態
-const db = mongoose.connection;
-// 連線異常
-db.on("error", () => {
-  console.log("mongodb error!");
-});
 // 連線成功
 db.once("open", () => {
-  console.log("mongodb connected!");
   Restaurant.create(restaurantList)
     .then(() => {
       console.log("restaurantSeeder done!");
